6c9c7ecf7d362190b04383025fcb07b39064603b
Satriodwip <satriodwipujianto@gmail.com>
2024-06-12 15:50:30 +0700

    pertemuan-13

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,35 @@
 <?php
-
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
 use App\Models\Review;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::resource('/movies', MovieController::class);
-Route::resource('/genres', GenreController::class);
-Route::resource('/reviews', ReviewController::class);
+
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/genres', GenreController::class);
+    Route::resource('/reviews', ReviewController::class);
+});
 
 
 
@@ -25,6 +39,6 @@ Route::resource('/reviews', ReviewController::class);
 
 
 
-// Route::get('/si02', function () {
-//     return "belajar laravel";
-// });
+
+
+require __DIR__.'/auth.php';
